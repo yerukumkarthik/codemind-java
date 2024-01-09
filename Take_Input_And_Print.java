af7070e7f2a8a74a9b9f9166ecbb718f560aa689
yerukumkarthik <119839102+yerukumkarthik@users.noreply.github.com>
2024-01-09 11:21:56 +0530

    import java.util.*; public class inp{     public static void main(String[] args)     {         Scanner read= new Scanner(System.in);         String st=read.nextLine();         System.out.print(st);     } }

---
+++ Take_Input_And_Print.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class inp{
+    public static void main(String[] args)
+    {
+        Scanner read= new Scanner(System.in);
+        String st=read.nextLine();
+        System.out.print(st);
+    }
+}
