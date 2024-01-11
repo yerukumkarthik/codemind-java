2e59202ab4eb9be5d4b84553dd3e81f0270e27c1
yerukumkarthik <119839102+yerukumkarthik@users.noreply.github.com>
2024-01-11 10:38:14 +0530

    import java.util.*; public class Frames{     public static void main(String[] args)     {         Scanner read=new Scanner(System.in);         int a=read.nextInt();         System.out.print(a*a+" "+a*4);     } }

---
+++ Area_and_Perimeter_of_Square.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Frames{
+    public static void main(String[] args)
+    {
+        Scanner read=new Scanner(System.in);
+        int a=read.nextInt();
+        System.out.print(a*a+" "+a*4);
+    }
+}
