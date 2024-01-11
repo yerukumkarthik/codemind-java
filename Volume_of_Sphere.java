3b263eb84f945f576b61d92e943a4affa5428197
yerukumkarthik <119839102+yerukumkarthik@users.noreply.github.com>
2024-01-11 10:42:03 +0530

    import java.util.*; public class Frames{     public static void main(String[] args)     {         Scanner read=new Scanner(System.in);         int a=read.nextInt();         System.out.printf("%.2f",(4*3.14*a*a*a)/3);     } }

---
+++ Volume_of_Sphere.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Frames{
+    public static void main(String[] args)
+    {
+        Scanner read=new Scanner(System.in);
+        int a=read.nextInt();
+        System.out.printf("%.2f",(4*3.14*a*a*a)/3);
+    }
+}
